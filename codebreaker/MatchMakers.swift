4508a1691fe2fbb1babdb17f81948d92eeef9f34
KusalPriyanka <kusalpriyanka782@gmail.com>
2026-02-24 12:21:26 +0530

    Complete the codebreaker app implementation up to the lecture four

---
+++ codebreaker/MatchMakers.swift
@@ -0,0 +1,42 @@
+//
+//  MatchMakers.swift
+//  codebreaker
+//
+//  Created by Kusal Perera on 2026-02-23.
+//
+
+import SwiftUI
+
+enum Match {
+    case nomatch
+    case exact
+    case inexact
+}
+
+struct MatchMakers: View {
+    var matches: [Match]
+    
+    var body: some View {
+        HStack {
+            VStack {
+                matchMaker(peg: 0)
+                matchMaker(peg: 1)
+            }
+            VStack {
+                matchMaker(peg: 2)
+                matchMaker(peg: 3)
+            }
+        }
+    }
+    
+    func matchMaker(peg: Int) -> some View {
+        let exactCount: Int = matches.count(where: { match in match == .exact })
+        let foundCount: Int = matches.count(where: { match in match != .nomatch })
+        
+        return Circle()
+            .fill(exactCount > peg ? Color.primary : Color.clear)
+            .strokeBorder(foundCount > peg ? Color.primary : Color.clear, lineWidth: 2)
+            .aspectRatio(1, contentMode: .fit)
+        
+    }
+}
---
+++ codebreaker/codebreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  codebreakerApp.swift
+//  codebreaker
+//
+//  Created by Kusal Perera on 2026-02-23.
+//
+
+import SwiftUI
+
+@main
+struct codebreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CodeBreakerView()
+        }
+    }
+}
